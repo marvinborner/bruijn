34d940048af27ca42b85a8a63d2e48fbefd4e63d
Marvin Borner <git@marvinborner.de>
2024-07-29 19:29:01 +0200

    Added actual source to definition

--- std/generate_map.sh
+++ std/generate_map.sh
@@ -3,7 +3,7 @@
 FILES="$(find * -type f -name "*.bruijn" ! -name "All.bruijn" ! -path "*Generic*")"
 
 list_defs() {
-	grep -Po "^[^:# \t][^ \t]*" "$1"
+	grep -Po "^[^:# \t][^ \t]*" "$1" | sed -e "s#\\(.*\\)#{\"name\": \"\\1\", \"source\": \"$1\"}#g"
 	inputs="$(awk '/^:input/ {print $2}' "$1")"
 	for i in $inputs; do
 		list_defs "${i#std/}.bruijn"
@@ -13,7 +13,7 @@ list_defs() {
 {
 	for f in $FILES; do
 		echo "{\"$f\":"
-		list_defs "$f" | jq -R . | jq -s .
+		list_defs "$f" | sed 's/\\/\\\\/g' | jq -s .
 		echo "}"
 	done
 } | jq -s add
